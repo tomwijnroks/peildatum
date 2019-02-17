b76d13ef932e5cd39aa5bf71831960cf574245db
Tom Wijnroks <twx@fastmail.com>
2019-02-17 19:45:48 +0100

    Add total average

--- scripts/average.py
+++ scripts/average.py
@@ -11,29 +11,52 @@
 import csv
 from datetime import datetime
 
-# Set count and sum to zero.
-count = 0
-sum = 0
-
-# Loop trough columns 0 (unixtime) and 1 (value) and convert unixtime to human date and time.
-for row in csv.reader(open(input_file)):
-  date_ = datetime.utcfromtimestamp(float(row[0])).strftime('%Y%m%d')
-  value = float(row[1])
-
-  # Check if the date matches with the input_date.
-  if date_ == input_date:
-      # Calculate the average of the values and round to 2 decimals.
-      count = count + 1
-      sum = sum + value
-      average = round(sum / count, 2)
-
-# Print the results.
 # Define the date and files.
 input_date = '2019-01-01'
 input_file = ['bitstampEUR.csv', 'coinbaseEUR.csv', 'krakenEUR.csv']
 
+# Set the total file count and sum to zero.
+total_count = 0
+total_sum = 0
+
+# Loop trough the input files.
+for file in input_file:
+  # Set file count and sum to zero.
+  file_row = 0
+  file_sum = 0
+
+  # Loop trough columns 0 (unixtime) and 1 (value) and convert unixtime to human date.
+  for row in csv.reader(open(file)):
+    date_ = datetime.utcfromtimestamp(float(row[0])).strftime('%Y-%m-%d')
+    value = float(row[1])
+
+    # Check if the date matches with the input_date.
+    if date_ == input_date:
+      # Calculate the file average: sum values, devide by rows and round to 2 decimals.
+      file_row = file_row + 1
+      file_sum = file_sum + value
+      file_avg = round(file_sum / file_row, 2)
+
+    # If none of the rows matched the date set the file average to zero.
+    if file_row == 0:
+      file_avg = 0
+
+  # Print the file results.
+  print('-'*40)
+  print('Date    :', input_date)
+  print('File    :', file)
+  print('Average :', file_avg)
+
+  # Calulate the total average if the file average is not zero:
+  # Sum the average file values, devide by the number of files and round to 2 decimals.
+  if file_avg != 0:
+    total_count = total_count + 1
+    total_sum = total_sum + file_avg
+    total_avg = round(total_sum / total_count, 2)
+  else:
+    total_avg = 0
+
+# Print the total average value.
 print('-'*40)
-print('File    :', input_file)
-print('Date    :', input_date)
-print('Average :', average)
+print('Total average value:', total_avg)
 print('-'*40)
